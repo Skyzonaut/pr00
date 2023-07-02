8fb62ea75e5722a15c0fb740bb07f86802f4416c
lytwz <lytwzz@gmail.com>
2023-07-02 16:42:58 +0200

    contacts and posters form working -> database

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
+var contactsModel = require('../model/contacts.js')
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
     res.render("contacts", {title: "Contacts"});
 });
 
+
+router.post('/request', function (req, res, next) {
+    var data = req.body; // Access the POST data sent from the client
+ 
+    if (data.mail === "" || data.sujet === "" || data.message === "") {
+        return res.redirect('/contacts');
+    }
+
+    contactsModel.create(data.mail, data.sujet, data.message, function (result) {
+        res.redirect('/');
+    });
+});
 module.exports = router;
