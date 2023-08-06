4fe7fe56c93ece89bcec15bf59322a6332bd163d
Lucas <lmennessier95@gmail.com>
2023-08-06 21:52:11 +0200

    ventes pagination

--- model/ventes.js
+++ model/ventes.js
@@ -1,8 +1,9 @@
 var db = require('./db.js');
 module.exports = {
     
-    read: function (secteur, callback) {
-        db.query("SELECT DISTINCT * from polar_caisse_articles WHERE EnVente = 1 AND Secteur = ? AND (Stock > 0 OR Stock IS NULL) GROUP BY CodeCaisse", [secteur], function(err, results){
+    read: function (secteur, offset, callback) {
+
+        db.query("SELECT DISTINCT * from polar_caisse_articles WHERE EnVente = 1 AND Secteur = ? AND (Stock > 0 OR Stock IS NULL) LIMIT 50 OFFSET ?", [secteur, (offset-1) * 30], function(err, results){
             if(err) throw err;
             callback(results);
         });
--- routes/ventes.js
+++ routes/ventes.js
@@ -2,37 +2,37 @@ var express = require('express');
 var router = express.Router();
 var itemModel = require('../model/ventes.js')
 
-/* GET users listing. */
 router.get('/fournitures', function(req, res, next) {
 
-    itemModel.read(3, function(result) {
-        res.render('ventes', { title: "Catalogue", items: result });
+    var index = parseInt(req.query.index);
+    itemModel.read(3, index, function(result) {
+        res.render('ventes', { title: "Catalogue", items: result, current: index, from: index -3, to: index +4 });
     })
 
 });
 
 router.get('/informatique', function(req, res, next) {
 
-    itemModel.read(7, function(result) {
-        res.render('ventes', { title: "Catalogue", items: result });
+    var index = parseInt(req.query.index);
+    itemModel.read(7, index, function(result) {
+        res.render('ventes', { title: "Catalogue", items: result, current: index,from: index -3, to: index +4 });
     })
-
 });
 
 router.get('/services', function(req, res, next) {
 
-    itemModel.read(1, function(result) {
-        res.render('ventes', { title: "Catalogue", items: result });
+    var index = parseInt(req.query.index);
+    itemModel.read(1, index, function(result) {
+        res.render('ventes', { title: "Catalogue", items: result, current: index, from: index -3, to: index +4 });
     })
-
 });
 
 router.get('/impressions', function(req, res, next) {
 
-    itemModel.read(5, function(result) {
-        res.render('ventes', { title: "Catalogue", items: result });
+    var index = parseInt(req.query.index);
+    itemModel.read(5, index, function(result) {
+        res.render('ventes', { title: "Catalogue", items: result, current: index, from: index -3, to: index +4 });
     })
-
 });
 
 module.exports = router;
