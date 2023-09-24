8eb821f2a0768ead20c2058128095b7558410ce6
Skyzonaut <40797024+Skyzonaut@users.noreply.github.com>
2023-09-24 15:33:27 +0200

    Update db.js

--- model/db.js
+++ model/db.js
@@ -1,6 +1,6 @@
 var mysql = require("mysql");
 var pool = mysql.createPool({
-host: "pr00.mysql.database.azure.com", //ou localhost
+host: "polardb.mysql.database.azure.com", //ou localhost
 user: "polar_admin",
 password: "P@ssword",
 database: "pr00"
