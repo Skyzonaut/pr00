567db80b297e9ca494c85a0d0828ea9283f51173
yohan <nogeek.lol@gmail.com>
2023-09-24 13:33:36 +0200

    Changement des credentials de la bdd pour la mise en ligne

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const bodyParser = require('body-parser');
+const http = require("http");
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
--- model/db.js
+++ model/db.js
@@ -1,8 +1,8 @@
 var mysql = require("mysql");
 var pool = mysql.createPool({
-host: "localhost", //ou localhost
-user: "root",
-password: "",
+host: "pr00.mysql.database.azure.com", //ou localhost
+user: "polar_admin",
+password: "P@ssword",
 database: "pr00"
 });
 module.exports = pool;  
