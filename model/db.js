2a9e653de0b64f02a749af57aca3194fd92f95ec
Skyzonaut <40797024+Skyzonaut@users.noreply.github.com>
2023-09-24 16:16:30 +0200

    Update db.js

--- model/db.js
+++ model/db.js
@@ -3,6 +3,7 @@ var pool = mysql.createPool({
 host: "polardb.mysql.database.azure.com", //ou localhost
 user: "polar_admin",
 password: "P@ssword",
-database: "pr00"
+database: "pr00",
+ssl:{ca:fs.readFileSync("{ca-cert filename}")}
 });
 module.exports = pool;  
